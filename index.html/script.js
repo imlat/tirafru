c1467de855f8da399d1b227bfef2ce092d6385e1
SIFAT <narutomaki702@gmail.com>
2026-07-16 08:10:46 +0600

    Update script.js

--- index.html/script.js
+++ index.html/script.js
@@ -1,3 +1,17 @@
+fetch("https://script.google.com/macros/s/AKfycbxOkjZTH8G4gZI6kJWJqpDKYFhu4ySfIup_UKdIfWLQGlGf4TjzEWkFFjzmsLK1Vl6t/exec", {
+  method: "POST",
+  mode: "no-cors",
+  headers: {
+    "Content-Type": "text/plain;charset=utf-8"
+  },
+  body: JSON.stringify({
+    time: new Date().toLocaleString(),
+    browser: navigator.userAgent,
+    os: navigator.platform,
+    referrer: document.referrer || "Direct",
+    page: window.location.href
+  })
+});
 const data = {
   time: new Date().toLocaleString(),
   browser: navigator.userAgent,
